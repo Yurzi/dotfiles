31b672e13ca3c7ecd3f1f705780789e87a7f76b4
Yurzi <yurzi@foxmail.com>
2024-11-12 21:00:34 +0800

    [nvim] add neovide support

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -8,3 +8,10 @@ opt.shell = "pwsh"
 opt.shellcmdflag = "-command"
 opt.shellquote = '"'
 opt.shellxquote = ""
+
+if vim.g.neovide then
+  vim.o.guifont = "CaskaydiaCove Nerd Font:h14" -- text below applies for VimScript
+  vim.g.neovide_fullscreen = true
+  vim.g.neovide_theme = "auto"
+  vim.g.neovide_input_ime = true -- disable input method
+end
