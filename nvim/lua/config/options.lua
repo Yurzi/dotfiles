a272d231e0e646e791fb234a0b3453acb797d996
Yurzi <yurzi@foxmail.com>
2024-11-15 01:00:20 +0800

    [nvim] add remote neovide support

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -8,9 +8,9 @@ opt.shell = "pwsh"
 opt.shellcmdflag = "-command"
 opt.shellquote = '"'
 opt.shellxquote = ""
+vim.o.guifont = "CaskaydiaCove Nerd Font:h14" -- text below applies for VimScript
 
 if vim.g.neovide then
-  vim.o.guifont = "CaskaydiaCove Nerd Font:h14" -- text below applies for VimScript
   vim.g.neovide_fullscreen = true
   vim.g.neovide_theme = "auto"
   vim.g.neovide_input_ime = true -- disable input method
