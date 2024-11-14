7fb8100b67c9772bc53abffff5b4ac81148b9a2c
Yurzi <yurzi@foxmail.com>
2024-11-15 01:07:56 +0800

    [nvim] add project plugin

--- nvim/lua/plugins/dashboard.lua
+++ nvim/lua/plugins/dashboard.lua
@@ -11,17 +11,5 @@ return {
   ]]
 
     logo = string.rep("\n", 8) .. logo .. "\n\n"
-
-    opts.config.header = vim.split(logo, "\n")
-    local projects = {
-      action = pick,
-      desc = " Projects",
-      icon = " ",
-      key = "p",
-    }
-    projects.desc = projects.desc .. string.rep(" ", 43 - #projects.desc)
-    projects.key_format = "  %s"
-
-    table.insert(opts.config.center, 3, projects)
   end,
 }
