bda37bb7f9b5e90becb03241e0e1a3c5a668f01c
Yurzi <yurzi@foxmail.com>
2024-11-15 01:09:21 +0800

    [nvim] add neovide support & add project plugin

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,12 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- add neovide support
+vim.o.guifont = "CaskaydiaCove Nerd Font:h14" -- text below applies for VimScript
+
+if vim.g.neovide then
+  vim.g.neovide_fullscreen = true
+  vim.g.neovide_theme = "auto"
+  vim.g.neovide_input_ime = true -- disable input method
+end
