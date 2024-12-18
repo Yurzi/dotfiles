694ed3e345953878b5a7c920025c78d91fc45795
Yurzi <yurzi@foxmail.com>
2024-12-18 09:32:35 +0800

    [nvim] set blink to supertab

---
+++ nvim/lua/plugins/blink.lua
@@ -0,0 +1,16 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+    keymap = {
+      preset = "super-tab",
+    },
+    completion = {
+      accept = {
+        -- experimental auto-brackets support
+        auto_brackets = {
+          enabled = false,
+        },
+      },
+    },
+  },
+}
